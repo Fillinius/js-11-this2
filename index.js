0ceafdf5ffc7cd44ddee4a4088154623c4c63041
Fillinius <113914362+Fillinius@users.noreply.github.com>
2022-11-30 12:03:37 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const dog = {
+   name: 'Чарли',
+   type: 'Собака',
+   makeSound() {
+      return 'Гав-Гав';
+   }
+}
+
+const bird = {
+   name: 'Петя',
+   type: 'Воробей',
+   makeSound() {
+      return 'Чик-чирик';
+   }
+}
+
+function makeDomestic(isDomestic) {
+   console.log(`name: ${this.name}, type: ${this.type}, ${this.makeSound},isDomestic: ${isDomestic}`)
+   console.log(`${this.type} по имени ${this.name} говорит ${this.makeSound()}`)
+   this.animal = isDomestic
+   if(isDomestic===false){
+    return isDomestic
+   }
+}
+
+const newMakeDomestic = makeDomestic.bind (dog, true)
+newMakeDomestic()
+
+makeDomestic.call(bird, false)
+makeDomestic.apply(bird, [false])
+console.log(bird)
+console.log(dog)
