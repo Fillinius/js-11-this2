6156890aa28345b830b33ad78eaab025faba2ab0
Fillinius <113914362+Fillinius@users.noreply.github.com>
2022-11-30 15:50:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function makeDomestic(isDomestic) {
    console.log(`name: ${this.name}, type: ${this.type}, ${this.makeSound},isDomestic: ${isDomestic}`)
    console.log(`${this.type} по имени ${this.name} говорит ${this.makeSound()}`)
    this.animal = isDomestic
-   if(isDomestic===false){
-    return isDomestic
-   }
+   // if(isDomestic===false){
+   //  return this.isDomestic
+   // } не работает проверка дамашнее живитное или нет. Или такую проверку делать не надо?
 }
 
 const newMakeDomestic = makeDomestic.bind (dog, true)
